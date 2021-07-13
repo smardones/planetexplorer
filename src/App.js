97237f8a6f5b1ce4bcaba4b8ccd7e2677c36844f
smardone <shaun.mardones@gmail.com>
2021-07-13 14:21:26 -0400

    created initial nav and planet page structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import planetData from './data';
 
 function App() {
   return (
---
+++ src/components/nav.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function NavBar() {
+    return(
+        <div>
+            <nav>
+                <h2>THE PLANETS</h2>
+                <ul className="nav-list">
+                    <li>MERCURY</li>
+                    <li>VENUS</li>
+                    <li>EARTH</li>
+                    <li>MARS</li>
+                    <li>JUPITER</li>
+                    <li>SATURN</li>
+                    <li>URANUS</li>
+                    <li>NEPTUNE</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar;
