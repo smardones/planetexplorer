c762ac7083e92c11408705cbae731fba7410c0fc
smardone <shaun.mardones@gmail.com>
2021-07-13 23:57:19 -0400

    implemented react-router-dom to allow traversal between pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import './App.css';
 import planetData from './data';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// Planet Pages
 import Mercury from './pages/mercury';
+import Venus from './pages/venus';
+import Earth from './pages/earth';
+import Mars from './pages/mars';
+import Jupiter from './pages/jupiter';
+import Saturn from './pages/saturn';
+import Uranus from './pages/uranus';
+import Neptune from './pages/neptune';
 
 
 function App() {
   return (
     <div className="App">
-        <Mercury props={planetData} />
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Mercury props={planetData} />
+            </Route>
+            <Route exact path="/venus">
+              <Venus props={planetData} />
+            </Route>
+            <Route exact path="/earth">
+              <Earth props={planetData} />
+            </Route>
+            <Route exact path="/mars">
+              <Mars props={planetData} />
+            </Route>
+            <Route exact path="/jupiter">
+              <Jupiter props={planetData} />
+            </Route>
+            <Route exact path="/saturn">
+              <Saturn props={planetData} />
+            </Route>
+            <Route exact path="/uranus">
+              <Uranus props={planetData} />
+            </Route>
+            <Route exact path="/neptune">
+              <Neptune props={planetData} />
+            </Route>
+
+          </Switch>
+
+        </Router>
     </div>
   );
 }
