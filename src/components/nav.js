142a242485cb6043d07d6269a8248facb3fd3529
smardone <shaun.mardones@gmail.com>
2021-07-15 15:20:20 -0400

    created sliding mobile menu

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,16 +1,47 @@
-import React from 'react';
-import MenuIcon from '../assets/icon-hamburger.svg'
+import React, { useState } from 'react';
+import {Link} from 'react-router-dom';
+import MenuIcon from '../assets/icon-hamburger.svg';
+
+import SideBar from './sidebarmenu';
 
 function NavBar() {
 
-    
+    const [ isMenuOpen, toggleMenu ] = useState(false);
+
 
     return(
         <nav>
             <h1 id="page-title">THE PLANETS</h1>  
-            <button id="menu-toggle"> 
+            <ul id="menu-choices">
+                <li>
+                    <Link to='/'>MERCURY</Link>
+                </li>
+                <li>
+                    <Link to='/venus'>VENUS</Link>
+                </li>
+                <li>
+                    <Link to='/earth'>EARTH</Link>
+                </li>
+                <li>
+                    <Link to='/mars'>MARS</Link>
+                </li>
+                <li>
+                    <Link to='/jupiter'>JUPITER</Link>
+                </li>
+                <li>
+                    <Link to='/saturn'>SATURN</Link>
+                </li>
+                <li>
+                    <Link to='/uranus'>URANUS</Link>
+                </li>
+                <li>
+                    <Link to='/neptune'>NEPTUNE</Link>
+                </li>
+            </ul>
+            <button id="menu-toggle" onClick={() => toggleMenu(!isMenuOpen)}> 
                 <img src={MenuIcon} />
             </button>
+            <SideBar isMenuOpen={isMenuOpen} toggleMenu={toggleMenu}/>
         </nav>
         
     )
---
+++ src/components/sidebarmenu.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import MenuIcon from '../assets/icon-hamburger.svg';
+
+function SideBar(props) {
+
+   let isMenuOpen = props.isMenuOpen;
+   let toggleMenu = props.toggleMenu;
+
+    return(
+        <div  id="sidebar" className={(isMenuOpen ? 'sidebar-open' : 'sidebar-closed')}>
+            <div id="sidebar-header">
+                <h1 id="page-title">THE PLANETS</h1>
+                    <button id="menu-toggle" onClick={() => toggleMenu(!isMenuOpen)}> 
+                <img src={MenuIcon} />
+            </button>
+            </div>
+            <ul>
+                <li className="sb-menu-item">
+                    <Link to='/'>MERCURY</Link>
+                </li>
+                <li className="sb-menu-item">
+                    <Link to='/venus'>VENUS</Link>
+                </li>
+                <li className="sb-menu-item">
+                    <Link to='/earth'>EARTH</Link>
+                </li>
+                <li className="sb-menu-item">
+                    <Link to='/mars'>MARS</Link>
+                </li>
+                <li className="sb-menu-item">
+                    <Link to='/jupiter'>JUPITER</Link>
+                </li>
+                <li className="sb-menu-item">
+                    <Link to='/saturn'>SATURN</Link>
+                </li>
+                <li className="sb-menu-item">
+                    <Link to='/uranus'>URANUS</Link>
+                </li>
+                <li className="sb-menu-item">
+                    <Link to='/neptune'>NEPTUNE</Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default SideBar;
