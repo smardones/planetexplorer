000b3e24fbecd2c7a96eb9f6affd87684ed5be4a
smardone <shaun.mardones@gmail.com>
2021-07-20 23:09:58 -0400

    completed general desktop formatting

--- src/pages/mercury.js
+++ src/pages/mercury.js
@@ -123,6 +123,61 @@ function Mercury(props) {
             {/* Here is the desktop sized JSX */}
             <section id="desktop">
 
+                {/* Visible when Overview is the active tab */}
+
+                <div id="overview" className={[isCurrentTab(currentTab, "overview") ? 'tab-content-active' : 'tab-content-hidden', 'main-content'].join(' ')}>
+                    <div className="image-container">
+                        <img id="mercury-image" src={PlanetImage} alt='Mercury Graphic'/>
+                    </div>
+                    <div className="desktop-content">
+                        <div className="text-block" >
+                            <h2>{planetInfo.name.toUpperCase()}</h2>
+                            <p className="information">{planetInfo.overview.content}</p>
+                            <p className="source">
+                                Source: <a className="source-link" href={planetInfo.overview.source}>Wikipedia</a>
+                            </p>
+                        </div>
+                        <MobileTabs setCurrentTab={setCurrentTab} />
+                    </div>
+                </div>
+
+                {/* Visible when Structure is the active tab */}
+
+                <div id="structure" className={[isCurrentTab(currentTab, "structure") ? 'tab-content-active' : 'tab-content-hidden', 'main-content'].join(' ')}>
+                    <div className="image-container">
+                        <img id="mercury-image" src={PlanetStructureImage} alt='Mercury Graphic'/>
+                    </div>
+                    <div className="desktop-content">
+                        <div className="text-block">
+                            <h2>{planetInfo.name.toUpperCase()}</h2>
+                            <p className="information">{planetInfo.structure.content}</p>
+                            <p className="source">
+                                Source: <a className="source-link" href={planetInfo.structure.source}>Wikipedia</a>
+                            </p>
+                        </div>
+                        <MobileTabs setCurrentTab={setCurrentTab} />
+                    </div>
+                </div>
+
+                {/* Visible when Surface is the active tab */}
+                
+                <div id="surface" className={[isCurrentTab(currentTab, "surface") ? 'tab-content-active' : 'tab-content-hidden', 'main-content'].join(' ')}>
+                    <div className="image-container">
+                        <img id="mercury-image" src={PlanetImage} alt='Mercury Graphic'/>
+                        <img className="geology-img" src={SurfaceImg} alt='Mercury Geological Graphic'/>
+                    </div>
+                    <div className="desktop-content">
+                        <div className="text-block">
+                            <h2>{planetInfo.name.toUpperCase()}</h2>
+                            <p className="information">{planetInfo.geology.content}</p>
+                            <p className="source">
+                                Source: <a className="source-link" href={planetInfo.geology.source}>Wikipedia</a>
+                            </p>
+                        </div>
+                        <MobileTabs setCurrentTab={setCurrentTab} />
+                    </div>
+                </div>
+
             </section>
             <Statistics props={planetInfo} />
         </body>
