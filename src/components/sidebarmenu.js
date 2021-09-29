0351f185630ef7b082c513d10280da9a15114692
smardone <shaun.mardones@gmail.com>
2021-09-28 22:07:32 -0400

    completed dots

--- src/components/sidebarmenu.js
+++ src/components/sidebarmenu.js
@@ -17,28 +17,35 @@ function SideBar(props) {
             </div>
             <ul>
                 <li className="sb-menu-item">
-                    <span id="mercury-dot"></span>
+                    <span id="mercury-dot" className="dot"></span>
                     <Link to='/'>MERCURY</Link>
                 </li>
                 <li className="sb-menu-item">
+                    <span id="venus-dot" className="dot"></span>
                     <Link to='/venus'>VENUS</Link>
                 </li>
                 <li className="sb-menu-item">
+                    <span id="earth-dot" className="dot"></span>
                     <Link to='/earth'>EARTH</Link>
                 </li>
                 <li className="sb-menu-item">
+                    <span id="mars-dot" className="dot"></span>
                     <Link to='/mars'>MARS</Link>
                 </li>
                 <li className="sb-menu-item">
+                    <span id="jupiter-dot" className="dot"></span>
                     <Link to='/jupiter'>JUPITER</Link>
                 </li>
                 <li className="sb-menu-item">
+                    <span id="saturn-dot" className="dot"></span>
                     <Link to='/saturn'>SATURN</Link>
                 </li>
                 <li className="sb-menu-item">
+                    <span id="uranus-dot" className="dot"></span>
                     <Link to='/uranus'>URANUS</Link>
                 </li>
                 <li className="sb-menu-item">
+                    <span id="neptune-dot" className="dot"></span>
                     <Link to='/neptune'>NEPTUNE</Link>
                 </li>
             </ul>
